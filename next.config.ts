c9b093f836ae04b8550466d5e842001395410813
Ira Henke <irahenke@Iras-Mac-mini.local>
2025-11-30 13:57:20 -0500

    Remove invalid config option

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
