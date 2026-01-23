678de3a4f38638d86f0961df84dd9e8f815aaf5e
Ira Henke <irahenke@Iras-Mac-mini.local>
2026-01-22 21:01:22 -0500

    Add CSS reset for Friends link in globals.css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -263,20 +263,18 @@ export default function Header({ user, userUsername, userAvatarUrl, searchCompon
           </InstantTooltip>
         ) : null}
 
-        {/* Friends - using span wrapper to avoid any link/button styling issues */}
+        {/* Friends */}
         {user && userUsername && (
           <InstantTooltip text="Friends">
-            <span
-              onClick={() => window.location.href = `/profile/${userUsername}?showFriends=true`}
+            <Link
+              href={`/profile/${userUsername}?showFriends=true`}
               style={{
-                display: "flex",
+                display: "inline-flex",
                 alignItems: "center",
                 justifyContent: "center",
                 width: 40,
                 height: 40,
                 borderRadius: "50%",
-                cursor: "pointer",
-                background: "transparent",
               }}
               onMouseEnter={(e) => {
                 e.currentTarget.style.background = "rgba(240, 235, 224, 0.1)";
@@ -288,7 +286,7 @@ export default function Header({ user, userUsername, userAvatarUrl, searchCompon
               <svg width="24" height="24" viewBox="0 0 24 24" fill="#c9a227">
                 <path d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm-9-2V7H4v3H1v2h3v3h2v-3h3v-2H6zm9 4c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
               </svg>
-            </span>
+            </Link>
           </InstantTooltip>
         )}
 
